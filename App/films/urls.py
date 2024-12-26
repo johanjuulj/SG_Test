ea6b98bb4a6ce508b78a0ad753cf215b262638da
Johan <johanjuulj@gmail.com>
2024-12-26 22:04:33 +0100

    added empty path

--- App/films/urls.py
+++ App/films/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'), 
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path("register/", views.RegisterView.as_view(), name="register"),
